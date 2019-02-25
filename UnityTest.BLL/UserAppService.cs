f8e952c5bee088348ba455820ebf4d63aa85cd7f
nemo <nemo.yuan@git-touch.com>
2019-02-25 17:29:44 +0800

    Create

---
+++ UnityTest.BLL/UserAppService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityTest.IBLL;
+
+namespace UnityTest.BLL
+{
+    public class UserAppService : IUserAppService
+    {
+        public string GetUser(string userName)
+        {
+            return "BLL User";
+        }
+    }
+}
